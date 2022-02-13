d06c00d73a655c7ebd39f8796d71b9a8f3d12948
Ned Batchelder <ned@nedbatchelder.com>
2022-02-12 21:13:05 -0500

    comment retrieval

--- issues.py
+++ issues.py
@@ -1,6 +1,7 @@
 import asyncio
 import itertools
 import json
+import operator
 import os
 
 from glom import glom
@@ -12,13 +13,16 @@ client = GraphqlClient(
     headers={"Authorization": f"Bearer {TOKEN}"},
 )
 
-QUERY = """\
+AUTHOR_DATA_FRAGMENT = """\
 fragment authorData on Actor {
     login
     url
     avatarUrl
 }
-query getIssuesWithComments(
+"""
+
+ISSUES_QUERY = """\
+query getIssues(
     $owner: String!
     $name: String!
     $since: String!
@@ -31,14 +35,17 @@ query getIssuesWithComments(
                 endCursor
             }
             nodes {
+                number
                 url
                 title
+                state
                 createdAt
+                updatedAt
                 body
                 author {
                     ...authorData
                 }
-                comments (last: 20) {
+                comments (last: 100) {
                     totalCount
                     nodes {
                         body
@@ -51,11 +58,42 @@ query getIssuesWithComments(
         }
     }
 }
-"""
+""" + AUTHOR_DATA_FRAGMENT
+
+COMMENTS_QUERY = """\
+query getIssueComments(
+    $owner: String!
+    $name: String!
+    $number: Int!
+    $after: String
+) {
+    repository(owner: $owner, name: $name) {
+        issue (number: $number) {
+            comments (first: 100, after: $after) {
+                pageInfo {
+                    hasNextPage
+                    endCursor
+                }
+                nodes {
+                    body
+                    author {
+                        ...authorData
+                    }
+                }
+            }
+        }
+    }
+}
+""" + AUTHOR_DATA_FRAGMENT
 
 JSON_NAMES = (f"out_{i:02}.json" for i in itertools.count())
 
 async def gql_execute(query, variables=None):
+    """
+    Execute one GraphQL query, with logging and error handling.
+    """
+    args = ", ".join(f"{k}: {v!r}" for k, v in variables.items())
+    print(query.splitlines()[0] + args + ")")
     data = await client.execute_async(query=query, variables=variables)
     with open(next(JSON_NAMES), "w") as j:
         json.dump(data, j, indent=4)
@@ -63,15 +101,19 @@ async def gql_execute(query, variables=None):
         raise Exception(data["message"])
     if "errors" in data:
         err = data["errors"][0]
-        msg = f"GraphQl error: {err['message']}"
+        msg = f"GraphQL error: {err['message']}"
         if "path" in err:
             msg += f" @{'.'.join(err['path'])}"
-        loc = err["locations"][0]
-        msg += f", line {loc['line']} column {loc['column']}"
+        if "locations" in err:
+            loc = err["locations"][0]
+            msg += f", line {loc['line']} column {loc['column']}"
         raise Exception(msg)
     return data
 
 async def gql_nodes(query, path, variables=None):
+    """
+    Excecute a query, and follow the pagination to get all the nodes.
+    """
     nodes = []
     vars = dict(variables)
     while True:
@@ -83,6 +125,24 @@ async def gql_nodes(query, path, variables=None):
         vars["after"] = fetched["pageInfo"]["endCursor"]
     return nodes
 
-vars = dict(owner="nedbat", name="coveragepy", since="2020-01-01T00:00:00")
-coro = gql_nodes(query=QUERY, path="repository.issues", variables=vars)
-data = asyncio.run(coro)
+async def get_issues(repo, since):
+    owner, name = repo.split("/")
+    vars = dict(owner=owner, name=name, since=since)
+    issues = await gql_nodes(query=ISSUES_QUERY, path="repository.issues", variables=vars)
+
+    issues.sort(key=operator.itemgetter("updatedAt"))
+    queried_issues = []
+    issue_queries = []
+    for i, iss in enumerate(issues):
+        if iss["comments"]["totalCount"] > len(iss["comments"]["nodes"]):
+            vars = dict(owner=owner, name=name, number=iss["number"])
+            queried_issues.append(i)
+            issue_queries.append(gql_nodes(query=COMMENTS_QUERY, path="repository.issue.comments", variables=vars))
+    commentss = await asyncio.gather(*issue_queries)
+    for i, comments in zip(queried_issues, commentss):
+        issues[i]["comments"]["nodes"] = comments
+    return issues
+
+issues = asyncio.run(get_issues("nedbat/coveragepy", since="2022-01-01T00:00:00"))
+for iss in issues:
+    print(f"{iss['number']}: {iss['state']} {iss['updatedAt']} {iss['title']} [{len(iss['comments']['nodes'])}]")
